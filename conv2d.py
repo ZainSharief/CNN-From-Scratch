ecacf6fb4933e2fc3fb9c0a66723e619f593da8c
Zain Sharief <zain.sharief21@gmail.com>
2024-06-23 12:11:06 +0100

    Increased speed + improved clarity
    Sped up the correlate function, replaced the signal correlate function with a fast fourier approach and improved code clarity + comments

--- conv2d.py
+++ conv2d.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy.signal as signal
 from activation import *
 
 class conv2d:
@@ -20,7 +19,7 @@ class conv2d:
     '''
 
     def __init__(self, filters: int, kernel_size: tuple = (3,3), strides: int = 1, padding: str = 'valid', activation: str = None) -> None: 
-        # Initializes the convolutional layer with the given parameters
+    # Initializes the convolutional layer with the given parameters
 
         self.filters = filters
         self.kernel_size = kernel_size
@@ -29,7 +28,7 @@ class conv2d:
         self.activation = activation_func(activation)
 
     def init_params(self, input_size: tuple) -> None:
-        # Initialises the kernel & bias parameters for the convolution given the expected output size
+    # Initialises the kernel & bias parameters for the convolution given the expected output size
 
         self.input_size = input_size
         self.output, self.padding_amount = self.calculate_size(input_size)
@@ -38,7 +37,7 @@ class conv2d:
         self.bias_params = np.zeros((self.output.shape[1], self.output.shape[2], self.output.shape[3]))
 
     def calculate_size(self, input_size: tuple) -> tuple:
-        # Calculates the expected output size and the amount of padding required on each x and y
+    # Calculates the expected output size and the amount of padding required on each x and y
 
         if self.padding == 'valid':
             output_width = np.ceil((input_size[1] - self.kernel_size[0] + 1) / self.strides).astype(int)
@@ -52,59 +51,85 @@ class conv2d:
             pad_height = np.ceil(((output_height - 1) * self.strides + self.kernel_size[1] - input_size[2]) / 2).astype(int) 
             return np.zeros((input_size[0], output_height, output_width, self.filters)), (pad_height, pad_width)
 
-    def correlate(self, input, output, kernel_params, forward=True):
-        # General function to perform a correlation between a tensor and kernel
+    def correlate(self, input_tensor, kernel, output_tensor, strides=1):
+    # Performs a correlation between a tensor and kernel
 
-        image_patches = np.lib.stride_tricks.sliding_window_view(
-            x=input,
-            window_shape=(kernel_params.shape[0], kernel_params.shape[1], kernel_params.shape[2]),   
-            axis=(1, 2, 3)
-        )
+        # Creates a sliding window view of the input with the shape of the kernel
+        kernel_shape = kernel.shape[:3]
+        output_shape = output_tensor.shape[:3]
+        image_patches = np.lib.stride_tricks.sliding_window_view(input_tensor, kernel_shape, axis=(1, 2, 3))
 
-        if forward: 
-            image_patches = image_patches[:, ::self.strides, ::self.strides, :, :, :, :]
+        # Applies the strides to the image view 
+        image_patches = image_patches[:, ::strides, ::strides, :, :, :, :]
 
-        image_patches = image_patches.reshape(output.shape[0], output.shape[1], output.shape[2], -1)
-        kernel = kernel_params.reshape(-1, kernel_params.shape[3])
+        # Reshapes the image and kernel into the correct format for convolution
+        image_patches = image_patches.reshape(*output_shape, -1)
+        kernel = kernel.reshape(-1, kernel.shape[3])
         
-        for i in range(output.shape[3]):
-            output[:, :, :, i] = np.tensordot(image_patches, kernel[:, i], axes=([3], [0]))
+        # Convolutes the input tensor and kernel
+        output_tensor = np.tensordot(image_patches, kernel, axes=([3], [0]))
+
+        return output_tensor
+    
+    def fftconvolve(self, tensor1, tensor2):
+    # Performs a fast convolution between two tensors
+
+        # Transforms the tensors into the frequency domain
+        tensor1 = np.fft.fftn(tensor1, self.kernel_shape)
+        tensor2 = np.fft.fftn(tensor2, self.kernel_shape)
+
+        # Computes the inverse discrete fourier transform of the tensor
+        output = tensor1 * tensor2
+        output = np.fft.ifftn(output)
+
+        # Extracts the real part of the inverse
+        output = np.real(output)
 
         return output
 
     def __call__(self, input: np.ndarray) -> np.ndarray:
-        # Performs the convolution and applies the activation function
+    # Performs the forward pass on the input tensor
         
+        # Stores the input to be used in backpropagation
         self.input = input
+
+        # Applies padding to the input tensor and correlates it with the kernel 
         input = np.pad(input, pad_width=((0, 0), (self.padding_amount[0], self.padding_amount[0]), (self.padding_amount[1], self.padding_amount[1]), (0, 0))) 
-        self.output = self.correlate(input, self.output, self.kernel_params)
+        self.output = self.correlate(input, self.kernel_params, self.output, strides=self.strides)
+
+        # Applies the activation function on the output tensor
+        self.output = self.activation(self.output)
         
-        return self.activation(self.output)
+        return self.output
 
     def backward(self, output_gradient: np.ndarray, learning_rate: float) -> np.ndarray:
-        # Backwards function which updates the weight and bias parameters, calculates the input gradient for the next layer
+    # Backwards function which updates the weight and bias parameters, calculates the input gradient for the next layer
 
+        # Calculates the gradient of the activation function
         output_gradient = self.activation.backward(output_gradient)
-        kernel_gradient = np.zeros_like(self.kernel_params)
-        input_gradient = np.zeros_like(self.input)
+        output_shape = output_gradient.shape
+        input_gradient = np.zeros(self.input_size)
 
+        # Calculates the padding on the output gradient to match the input gradient
         pad_x = np.ceil((self.kernel_size[0] + input_gradient.shape[1] - output_gradient.shape[1] - 1) // 2).astype(int)
         pad_y = np.ceil((self.kernel_size[1] + input_gradient.shape[2] - output_gradient.shape[2] - 1) // 2).astype(int)
 
+        # Pads the output gradient and transposes the kernel for a full convolution
         padded_output_gradient = np.pad(output_gradient, ((0, 0), (pad_x, pad_x), (pad_y, pad_y), (0, 0)))
-        input_gradient = self.correlate(padded_output_gradient, input_gradient, self.kernel_params.transpose((1, 0, 3, 2)), forward=False)
+        kernel_params_transpose = self.kernel_params.transpose((1, 0, 3, 2))
+        input_gradient = self.correlate(padded_output_gradient, kernel_params_transpose, input_gradient)
 
+        # Computes the gradient of the unstrided input tensor
         if self.strides > 1:
-            output_strided = np.zeros((output_gradient.shape[0], output_gradient.shape[1] * self.strides, output_gradient.shape[2] * self.strides, output_gradient.shape[3]), dtype=np.float32)
+            output_strided = np.zeros((output_shape[0], output_shape[1] * self.strides, output_shape[2] * self.strides, output_shape[3]), dtype=np.float32)
             output_strided[:, 1::self.strides, 1::self.strides, :] = output_gradient
         else:
             output_strided = output_gradient
 
-        for batch in range(input_gradient.shape[0]):
-            for filter in range(self.filters):
-                for input_filter in range(self.input_size[3]):
-                    kernel_gradient[:, :, input_filter, filter] += signal.correlate2d(self.input[batch, :, :, input_filter], output_strided[batch, :, :, filter], mode='valid')
-       
+        # Calculates the kernel gradient using fast convolutions
+        kernel_gradient = self.fftconvolve(self.input, output_strided)
+
+        # Updates the kernel and bias parameters
         kernel_gradient /= input_gradient.shape[0]            
         self.kernel_params -= learning_rate * kernel_gradient
         self.bias_params -= learning_rate * np.mean(output_gradient, axis=0) 
